7bd8b4afb06ac7504e0bfa02158dba005c750a88
Kevin BESSON <kevin.besson.719@gmail.com>
2020-06-22 20:04:35 +0200

    added frontend to run without node server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.get("/api/test", (req, res) => res.json({"test": "testResult"}));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
