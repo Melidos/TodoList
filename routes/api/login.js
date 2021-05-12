490bb73d934840f0a88eab9f51e0a320942aa4e2
Kevin BESSON <kevin.besson.719@gmail.com>
2021-05-12 17:56:18 +0200

    Fix Heroku Logout

--- routes/api/login.js
+++ routes/api/login.js
@@ -26,7 +26,7 @@ router.post("/login", (req, res) => {
             });
 
             res
-              .cookie("authToken", token, { httpOnly: true, sameSite = 'none' })
+              .cookie("authToken", token, { httpOnly: true, sameSite: "none" })
               .status(200)
               .send(user);
           } else {
@@ -80,7 +80,7 @@ router.get("/disconnect", (req, res) => {
     .cookie("authToken", "", {
       expires: new Date("1970-01-01"),
       httpOnly: true,
-      sameSite = 'none'
+      sameSite: "none",
     })
     .status(200)
     .send();
